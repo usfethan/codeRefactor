0927a8e97185582ed01c005882118a7838482d2c
Fethan Us <77454834+usfethan@users.noreply.github.com>
2021-10-22 12:27:04 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,138 @@
+* {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+  
+  a {
+    color: #ffffff;
+    text-decoration: none;
+  }
+  
+  p {
+    font-size: 16px;
+  }
+  
+  body {
+    background-color: #d9dcd6;
+  }
+  
+  header {
+    padding: 20px;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+    background-color: #2a607c;
+    color: #ffffff;
+  }
+  
+  
+  
+  header h1 {
+    display: inline-block;
+    font-size: 48px;
+  }
+  
+  header h1 .seo {
+    color: #d9dcd6;
+  }
+  
+  header div {
+    padding-top: 15px;
+    margin-right: 20px;
+    float: right;
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+    font-size: 20px;
+  }
+  
+  header div ul {
+    list-style-type: none;
+  }
+  
+  header div ul li {
+    display: inline-block;
+    margin-left: 25px;
+  }
+  
+  /* header image*/
+  header section img {
+    height: 800px;
+    width: 100%;
+    margin-bottom: 25px;
+    background-size: cover;
+    background-position: center;
+  }
+  
+  .float-left {
+    float: left;
+    margin-right: 25px;
+  }
+  
+  .float-right {
+    float: right;
+    margin-left: 25px;
+  }
+  
+  div img {
+    max-height: 200px;
+  }
+
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  
+  main {
+    width: 75%;
+    display: inline-block;
+    margin-left: 20px;
+  }
+  
+  main div {
+    margin-bottom: 20px;
+    padding: 50px;
+    height: 300px;
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+    background-color: #0072bb;
+    color: #ffffff;
+  }
+  
+  aside {
+    margin-right: 20px;
+    padding: 20px;
+    clear: both;
+    float: right;
+    width: 20%;
+    height: 100%;
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+    background-color: #2589bd;
+  }
+  
+ 
+  h3 {
+    margin-bottom: 10px;
+    text-align: center;
+    margin-bottom: 32px;
+    color: #ffffff;
+  }
+  
+  aside div p {
+    margin-bottom: 32px;
+    color: #ffffff;
+  }
+
+  main aside img {
+    display: block;
+    margin: 10px auto;
+    max-width: 150px;
+  }
+
+.footer {
+    padding: 30px;
+    clear: both;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    text-align: center;
+}
+
+.footer h2 {
+    font-size: 20px;
+}
